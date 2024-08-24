6ded440849c07abf32cf39d828185729171d0b35
ryonakaya <nakayaryo0716@gmail.com>
2024-08-24 09:30:32 +0900

    [refacttoring] fix module dependency

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,21 +1,19 @@
-use app_state::AppState;
-use auth::UserDataDb;
-use rooms::RoomsDb;
+use app::AppState;
+use middleware::auth::UserDataDb;
+use models::rooms::RoomsDb;
 
-mod app_state;
-mod auth;
-mod handlers;
-mod rooms;
+mod app;
+mod controllers;
+mod middleware;
+mod models;
 mod router;
-mod session;
 mod utility;
-mod websocket;
 
 #[tokio::main]
 async fn main() {
     let rooms_db = RoomsDb::new();
     let users_pool = UserDataDb::new("database_url").await.unwrap();
-    
+
     let state = AppState::new(rooms_db, users_pool);
     let app = router::app(state);
 
