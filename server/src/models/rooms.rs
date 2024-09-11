afdaae274df175f0e47fdf30b0bfbfc263bc6792
ryonakaya <nakayaryo0716@gmail.com>
2024-09-11 23:37:59 +0900

    [fix] chenge to  Room have Tuple Struct

--- server/src/models/rooms.rs
+++ server/src/models/rooms.rs
@@ -10,17 +10,26 @@ use uuid::Uuid;
 
 use crate::app::AppState;
 
+#[derive(Debug, Clone, Eq, Hash, PartialEq, Serialize, Deserialize)]
+pub struct RoomId(String);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RoomName(String);
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RoomTime(u32);
+
 #[derive(Debug, Clone, Deserialize)]
 pub struct CreateRoom {
-    room_name: String,
-    room_time: u32,
+    room_name: RoomName,
+    room_time: RoomTime,
 }
 
 #[derive(Debug, Clone)]
 pub struct Room {
-    room_id: String,
-    room_name: String,
-    room_time: u32,
+    room_id: RoomId,
+    room_name: RoomName,
+    room_time: RoomTime,
     sender: Sender<String>,
 }
 
@@ -28,23 +37,23 @@ impl Room {
     pub fn new(create_room: CreateRoom) -> Self {
         let room_id = Uuid::new_v4().to_string();
         Self {
-            room_id,
+            room_id: RoomId(room_id),
             room_name: create_room.room_name,
             room_time: create_room.room_time,
             sender: Sender::new(128),
         }
     }
 
-    pub fn get_room_id(&self) -> &str {
+    pub fn get_room_id(&self) -> &RoomId {
         &self.room_id
     }
 
-    pub fn get_room_name(&self) -> &str {
+    pub fn get_room_name(&self) -> &RoomName {
         &self.room_name
     }
 
-    pub fn get_room_time(&self) -> u32 {
-        self.room_time
+    pub fn get_room_time(&self) -> &RoomTime {
+        &self.room_time
     }
 
     pub fn get_sender(&self) -> Sender<String> {
@@ -54,24 +63,24 @@ impl Room {
 
 #[derive(Debug, Clone, Serialize)]
 pub struct RoomInfo {
-    room_id: String,
-    room_name: String,
-    room_time: u32,
+    room_id: RoomId,
+    room_name: RoomName,
+    room_time: RoomTime,
 }
 
 impl RoomInfo {
     pub fn new(room: Room) -> Self {
         Self {
-            room_id: room.get_room_id().to_string(),
-            room_name: room.get_room_name().to_string(),
-            room_time: room.get_room_time(),
+            room_id: room.get_room_id().to_owned(),
+            room_name: room.get_room_name().to_owned(),
+            room_time: room.get_room_time().to_owned(),
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct RoomsDb {
-    pool: Arc<Mutex<HashMap<String, Room>>>,
+    pool: Arc<Mutex<HashMap<RoomId, Room>>>,
 }
 
 impl RoomsDb {
@@ -81,8 +90,14 @@ impl RoomsDb {
         }
     }
 
-    pub fn pool_ref(&self) -> Arc<Mutex<HashMap<String, Room>>> {
+    pub fn pool_ref(&self) -> Arc<Mutex<HashMap<RoomId, Room>>> {
         Arc::clone(&self.pool)
+    }
+
+    pub fn get_room(&self, room_id: &RoomId) -> Result<Room, RoomError> {
+        let lock = self.pool.lock().map_err(|_e| RoomError::LockError)?;
+        let room = lock.get(room_id).ok_or(RoomError::NotFound)?.to_owned();
+        Ok(room)
     }
 }
 
@@ -95,7 +110,7 @@ impl FromRef<AppState> for RoomsDb {
 pub trait RoomsManage {
     fn create_room(&self, payload: CreateRoom) -> Result<Room, RoomError>;
     fn get_all_room(&self) -> Result<Vec<RoomInfo>, RoomError>;
-    fn delete_room(&self, room_id: String) -> Result<(), RoomError>;
+    fn delete_room(&self, room_id: RoomId) -> Result<(), RoomError>;
 }
 
 #[derive(Debug, Clone, Error)]
@@ -111,7 +126,7 @@ impl RoomsManage for RoomsDb {
         let new_room = Room::new(room_payload);
 
         let mut lock = self.pool.lock().map_err(|_e| RoomError::LockError)?;
-        lock.insert(new_room.get_room_id().to_string(), new_room.clone());
+        lock.insert(new_room.get_room_id().to_owned(), new_room.clone());
 
         Ok(new_room)
     }
@@ -129,7 +144,7 @@ impl RoomsManage for RoomsDb {
         Ok(rooms)
     }
 
-    fn delete_room(&self, room_id: String) -> Result<(), RoomError> {
+    fn delete_room(&self, room_id: RoomId) -> Result<(), RoomError> {
         self.pool
             .lock()
             .map_err(|_e| RoomError::LockError)?
