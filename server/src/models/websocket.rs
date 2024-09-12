8728d6f85bb2c4922df59dca3534840f6329321f
ryonakaya <nakayaryo0716@gmail.com>
2024-09-12 13:36:55 +0900

    [fix] chenge to function take Room

--- server/src/models/websocket.rs
+++ server/src/models/websocket.rs
@@ -1,38 +1,28 @@
-use crate::utility::acquire_lock;
 use axum::extract::ws::Message;
 use axum::extract::ws::WebSocket;
 use futures::{SinkExt, StreamExt};
 use tracing::warn;
 
-use super::rooms::RoomsDb;
+use super::rooms::Room;
 
-pub async fn websocket_task(socket: WebSocket, room_db: RoomsDb, room_id: String) {
-    let (mut sender, mut receiver) = socket.split();
+pub async fn websocket_task(socket: WebSocket, room: Room) {
+    let (mut ws_sender, mut ws_receiver) = socket.split();
 
-    let tx = match acquire_lock(&room_db.pool_ref()).and_then(|guard| guard.get(&room_id).cloned())
-    {
-        Some(room) => room.get_sender(),
-        None => return,
-    };
-
+    let tx = room.get_sender();
+    let tx_clone = tx.clone();
     let mut receive_task = tokio::task::spawn(async move {
-        while let Some(Ok(Message::Text(text))) = receiver.next().await {
-            if let Err(e) = tx.send(text) {
+        while let Some(Ok(Message::Text(text))) = ws_receiver.next().await {
+            if let Err(e) = tx_clone.send(text) {
                 warn!("[error]{:?}", e);
                 break;
             }
         }
     });
 
-    let mut rx =
-        match acquire_lock(&room_db.pool_ref()).and_then(|guard| guard.get(&room_id).cloned()) {
-            Some(room) => room.get_sender().subscribe(),
-            None => return,
-        };
-
+    let mut rx = tx.subscribe();
     let mut send_task = tokio::task::spawn(async move {
         while let Ok(text) = rx.recv().await {
-            if let Err(e) = sender.send(Message::Text(text)).await {
+            if let Err(e) = ws_sender.send(Message::Text(text)).await {
                 warn!("[error]:{:?}", e);
                 break;
             }
