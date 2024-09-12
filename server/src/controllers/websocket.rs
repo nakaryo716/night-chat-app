b564abe4e4ede11355b8cf452da775dcde26daf4
ryonakaya <nakayaryo0716@gmail.com>
2024-09-12 13:34:12 +0900

    [fix] chenge to select room before websocket upgrade

--- server/src/controllers/websocket.rs
+++ server/src/controllers/websocket.rs
@@ -1,18 +1,25 @@
 use axum::extract::{Path, State, WebSocketUpgrade};
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use tracing::{event, Level};
 
-use crate::models::rooms::RoomsDb;
+use crate::models::rooms::{RoomId, RoomsDb};
 use crate::models::websocket::websocket_task;
 
-pub async fn websocket_handler(
+pub async fn websocket_upgrade_handler(
     ws: WebSocketUpgrade,
     State(rooms_db): State<RoomsDb>,
     Path(room_id): Path<String>,
 ) -> impl IntoResponse {
-    ws.on_failed_upgrade(|e| {
-        let message = format!("error: [{:?}]", e);
-        event!(Level::WARN, message)
-    })
-    .on_upgrade(|socket| websocket_task(socket, rooms_db, room_id))
+    let room = rooms_db.get_room(&RoomId::new(room_id));
+
+    match room {
+        Ok(room) => ws
+            .on_failed_upgrade(|e| {
+                let message = format!("error: [{:?}]", e);
+                event!(Level::WARN, message)
+            })
+            .on_upgrade(|socket| websocket_task(socket, room)),
+        Err(_e) => StatusCode::NOT_FOUND.into_response(),
+    }
 }
