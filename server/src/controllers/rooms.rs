7d306956028351e35c91cbf37df98f38dc5540a0
ryonakaya <nakayaryo0716@gmail.com>
2024-09-12 13:34:42 +0900

    [impl] add delete_room handler

--- server/src/controllers/rooms.rs
+++ server/src/controllers/rooms.rs
@@ -1,6 +1,11 @@
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
 
-use crate::models::rooms::{CreateRoom, RoomInfo, RoomsDb, RoomsManage};
+use crate::models::rooms::{CreateRoom, RoomId, RoomInfo, RoomsDb};
 
 pub async fn create_room_handler(
     State(rooms_db): State<RoomsDb>,
@@ -17,8 +22,20 @@ pub async fn room_list_handler(
     State(rooms_db): State<RoomsDb>,
 ) -> Result<impl IntoResponse, StatusCode> {
     let rooms = rooms_db
-        .get_all_room()
+        .get_all_room_info()
         .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok((StatusCode::OK, Json(rooms)))
+}
+
+pub async fn delete_room_handler(
+    State(rooms_db): State<RoomsDb>,
+    Path(room_id_url): Path<String>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let room_id = RoomId::new(room_id_url);
+    rooms_db
+        .delete_room(room_id)
+        .map_err(|_e| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(StatusCode::NO_CONTENT)
 }
