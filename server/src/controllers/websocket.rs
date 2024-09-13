21b0339c23c22d3610d4f510a1396a83a5787fca
ryonakaya <nakayaryo0716@gmail.com>
2024-09-13 12:04:34 +0900

    [fix] chenge to handler get cookie(user_name)

--- server/src/controllers/websocket.rs
+++ server/src/controllers/websocket.rs
@@ -1,17 +1,25 @@
 use axum::extract::{Path, State, WebSocketUpgrade};
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
+use axum_extra::extract::CookieJar;
 use tracing::{event, Level};
 
 use crate::models::rooms::{RoomId, RoomsDb};
+use crate::models::user_name::get_user_name_from_cookie;
 use crate::models::websocket::websocket_task;
 
 pub async fn websocket_upgrade_handler(
     ws: WebSocketUpgrade,
     State(rooms_db): State<RoomsDb>,
     Path(room_id): Path<String>,
+    jar: CookieJar,
 ) -> impl IntoResponse {
     let room = rooms_db.get_room(&RoomId::new(room_id));
+
+    let user_name = match get_user_name_from_cookie(jar) {
+        Some(cookie_val) => cookie_val,
+        None => return StatusCode::UNAUTHORIZED.into_response(),
+    };
 
     match room {
         Ok(room) => ws
@@ -19,7 +27,7 @@ pub async fn websocket_upgrade_handler(
                 let message = format!("error: [{:?}]", e);
                 event!(Level::WARN, message)
             })
-            .on_upgrade(|socket| websocket_task(socket, room)),
+            .on_upgrade(move |socket| websocket_task(socket, room, user_name)),
         Err(_e) => StatusCode::NOT_FOUND.into_response(),
     }
 }
