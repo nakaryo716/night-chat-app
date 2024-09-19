36ddda94725b9b7fb2a8c77b89a4c36ddee2c3a1
ryonakaya <nakayaryo0716@gmail.com>
2024-09-19 22:23:41 +0900

    [impl] add new handler and comment

--- server/src/router.rs
+++ server/src/router.rs
@@ -5,12 +5,11 @@ use http::Method;
 use tower_http::cors::CorsLayer;
 
 use crate::app::AppState;
-use crate::controllers::rooms::{create_room_handler, delete_room_handler, room_list_handler};
+use crate::controllers::rooms::{create_room_handler, delete_room_handler, get_room_info_handler, room_list_handler};
 use crate::controllers::user_name::{
     delete_user_name_from_cookie_handler, get_user_name_from_cookie_handler,
     register_user_name_as_cookie_handler,
 };
-use crate::controllers::view::{index, user_view};
 use crate::controllers::websocket::websocket_upgrade_handler;
 use crate::middleware::time_limit::time_limit_check;
 
@@ -19,20 +18,58 @@ const ORIGINUR2: &str = "http://127.0.0.1:5173";
 
 pub fn app(state: AppState) -> Router {
     Router::new()
-        .route("/", get(index))
-        .route("/user", get(user_view))
+        // ------Set User name------
+        // METHOD : POST
+        // URI    : 'http://localhost:3000/user_name'
+        // PAYLOAD:
+        // {
+        //    "user_name": "user name here"
+        // }
+        //
+        // ------Get User name------
+        // METHOD : GET
+        // URI    : 'http://localhost:3000/user_name'
         .route(
             "/user_name",
             post(register_user_name_as_cookie_handler)
                 .get(get_user_name_from_cookie_handler)
                 .delete(delete_user_name_from_cookie_handler),
         )
+        //
+        //
+        // ------Create room------
+        // METHOD : POST
+        // URI    : 'http://localhost:3000/create_room'
+        // PAYLOAD:
+        // {
+        //    "room_name": "room name here"
+        // }
         .route("/create_room", post(create_room_handler))
+        //
+        //
+        // ------Get room infomation------
+        // METHOD : GET
+        // URI    : 'http://localhost:3000/room/:room_id'
+        .route("/room/:room_id", get(get_room_info_handler))
+        //
+        //
+        // ------Get all room infomation------
+        // METHOD : GET
+        // URI    : 'http://localhost:3000/room_ls/'
         .route("/room_ls", get(room_list_handler))
+        //
+        //
         .route("/delete_room/:room_id", delete(delete_room_handler))
         .route("/websocket/:room_id", get(websocket_upgrade_handler))
+        //
+        //
+        // Application State 
         .with_state(state)
+        //
+        // time limit layer  
         .route_layer(axum::middleware::from_fn(time_limit_check))
+        //
+        // cors layer
         .layer(
             CorsLayer::new()
                 .allow_origin([ORIGINURL.parse().unwrap(), ORIGINUR2.parse().unwrap()])
