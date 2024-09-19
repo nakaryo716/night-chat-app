84dce8b1d9f7b003e3a916f3b613449a66ce77d8
ryonakaya <nakayaryo0716@gmail.com>
2024-09-19 22:24:21 +0900

    [fix] delete view handler

--- server/src/controllers/view.rs
+++
@@ -1,9 +0,0 @@
-use axum::response::{Html, IntoResponse};
-
-pub async fn index() -> impl IntoResponse {
-    Html(include_str!("../../index.html"))
-}
-
-pub async fn user_view() -> impl IntoResponse {
-    Html(include_str!("../../user.html"))
-}
